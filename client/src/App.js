b0875868aaefc06476d236d90ab4f7016c0e73e2
Viktors Jenovs <jviktors@gmail.com>
2017-07-25 08:59:05 +0300

    Create sample fakeApi request from Main Page (#20)

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import api from './fakeApi';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: '',
+      data: [],
     };
   }
 
-  async componentDidMount() {
-    const json = await fetch('/api/test/').then(res => res.json());
-    this.setState(() => ({
-      data: json.data,
-    }));
+  componentDidMount() {
+    this.getItems();
+  }
+
+  async getItems() {
+    let json = [];
+    try {
+      json = await api.getItems();
+      this.setState(() => ({
+        data: json.data,
+      }));
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   render() {
+    const { data } = this.state;
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome</h2>
         </div>
-        <p className="App-intro">
-          Data from server: {this.state.data}
-        </p>
+        <div className="App-intro">
+          {!!data.length
+            ? data.map((item, i) =>
+                <div key={item.id}>
+                  <p>
+                    <strong>
+                      {item.companyName}
+                    </strong>
+                    {' - '}
+                    <span>
+                      {item.info.title}
+                    </span>
+                  </p>
+                </div>
+              )
+            : <div>Loading...</div>}
+        </div>
       </div>
     );
   }

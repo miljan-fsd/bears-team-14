247462fde2c79ec972896b43a9a89480637434d2
Viktors Jenovs <jviktors@gmail.com>
2017-07-25 18:18:45 +0300

    Update mock data (#23)

--- client/src/fakeApi/jobs.js
+++ client/src/fakeApi/jobs.js
@@ -1,42 +1,366 @@
-const job1 = {
-  id: 12,
-  companyId: 42,
-  companyName: 'Google Ltd.', // is populated on server from company table
-  info: {
-    title: 'This is a job',
-    description: 'A fancy and long job description',
-    imgUrl: 'https://url.to.free.image.png.com',
-    website: 'https://example.com',
-    social: [
-      { Twitter: 'https://twitter' },
-      { LinkedIn: 'https://linkedin.com' },
-    ],
+export default [
+  {
+    id: 1,
+    companyId: 'cGLSxH4',
+    companyName: 'Kamba',
+    info: {
+      title: 'Praesent blandit.',
+      description:
+        'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_bc58be392b90418392b7eb7990764104.jpg',
+      website: 'e-recht24.de',
+      social: 'hac habitasse platea',
+    },
+    tags: 'aliquam erat volutpat in congue',
+    isFeatured: true,
+    expDate: 1501859688982,
+    location: 'Colatina',
   },
-  tags: ['JavaScript', 'WebDev', 'Europe'],
-  isFeatured: true,
-  expDate: 1500959439702,
-  location: 'San Francisco, US', // populated on server???
-};
-
-const job2 = {
-  id: 14,
-  companyId: 42,
-  companyName: 'Google Ltd.', // is populated on server from company table
-  info: {
-    title: 'Bot creating guru',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    imgUrl: 'https://url.to.free.image.png.com',
-    website: 'https://example.com',
-    social: [
-      { Twitter: 'https://twitter' },
-      { LinkedIn: 'https://linkedin.com' },
-    ],
+  {
+    id: 2,
+    companyId: 'DoyFwH',
+    companyName: 'Tagtune',
+    info: {
+      title: 'Aliquam sit amet diam in magna bibendum imperdiet.',
+      description:
+        'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_8826dc25b58b45676833c6b1994c7553.jpg',
+      website: 'ask.com',
+      social: 'consequat metus sapien',
+    },
+    tags: 'odio elementum eu interdum eu tincidunt',
+    isFeatured: false,
+    expDate: 1501888940057,
+    location: 'Sandvika',
   },
-  tags: ['Python', 'Machine Learning', 'UK'],
-  isFeatured: false,
-  expDate: 1500959249702,
-  location: 'London, UK', // populated on server???
-};
-
-export default [job1, job2];
+  {
+    id: 3,
+    companyId: 'XmGU1YJ1JEr',
+    companyName: 'Fadeo',
+    info: {
+      title: 'Nullam molestie nibh in lectus.',
+      description:
+        'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.\n\nPraesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'webeden.co.uk',
+      social: 'faucibus accumsan',
+    },
+    tags: 'hac habitasse platea dictumst aliquam augue quam',
+    isFeatured: false,
+    expDate: 1501422427960,
+    location: 'Huangtan',
+  },
+  {
+    id: 4,
+    companyId: 'kttsqakvG4fT',
+    companyName: 'Buzzdog',
+    info: {
+      title: 'Donec posuere metus vitae ipsum.',
+      description:
+        'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_bc58be392b90418392b7eb7990764104.jpg',
+      website: 'nymag.com',
+      social: 'sed sagittis nam',
+    },
+    tags: 'lacus morbi sem mauris',
+    isFeatured: true,
+    expDate: 1501705585790,
+    location: 'Oliveira',
+  },
+  {
+    id: 5,
+    companyId: 'KrNyUvQvy0Ds',
+    companyName: 'Skinder',
+    info: {
+      title:
+        'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      description:
+        'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_536c0710b4804dba4e3b97995725b18e.jpg',
+      website: 'bbb.org',
+      social: 'at lorem',
+    },
+    tags: 'felis donec semper sapien a libero nam dui proin',
+    isFeatured: true,
+    expDate: 1501121390687,
+    location: 'Šenkovec',
+  },
+  {
+    id: 6,
+    companyId: '7An7CveBQb8',
+    companyName: 'Lazz',
+    info: {
+      title:
+        'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.',
+      description:
+        'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_bc58be392b90418392b7eb7990764104.jpg',
+      website: 'economist.com',
+      social: 'amet',
+    },
+    tags: 'aliquet massa',
+    isFeatured: false,
+    expDate: 1501827953532,
+    location: 'Khe Tre',
+  },
+  {
+    id: 7,
+    companyId: 'C9QjwJ5',
+    companyName: 'Fivechat',
+    info: {
+      title: 'Nulla mollis molestie lorem.',
+      description:
+        'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.\n\nNulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_bc58be392b90418392b7eb7990764104.jpg',
+      website: 'state.tx.us',
+      social: 'consequat lectus',
+    },
+    tags: 'nulla mollis molestie lorem quisque ut erat curabitur',
+    isFeatured: false,
+    expDate: 1501214831504,
+    location: 'Luokou',
+  },
+  {
+    id: 8,
+    companyId: 'Z9SmhhSKU',
+    companyName: 'Divavu',
+    info: {
+      title:
+        'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      description:
+        'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.\n\nPhasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_8826dc25b58b45676833c6b1994c7553.jpg',
+      website: 'foxnews.com',
+      social: 'massa donec dapibus',
+    },
+    tags: 'nulla ut erat id mauris vulputate elementum nullam',
+    isFeatured: false,
+    expDate: 1501169716751,
+    location: 'Anuchino',
+  },
+  {
+    id: 9,
+    companyId: 'QhvdqQkABNH',
+    companyName: 'Fatz',
+    info: {
+      title: 'In hac habitasse platea dictumst.',
+      description:
+        'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'spotify.com',
+      social: 'lacus at',
+    },
+    tags: 'nulla dapibus',
+    isFeatured: true,
+    expDate: 1501973233650,
+    location: 'Skarżysko-Kamienna',
+  },
+  {
+    id: 10,
+    companyId: 'vH8td4',
+    companyName: 'Skyndu',
+    info: {
+      title: 'Vivamus vel nulla eget eros elementum pellentesque.',
+      description:
+        'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_8826dc25b58b45676833c6b1994c7553.jpg',
+      website: 'ucsd.edu',
+      social: 'quisque',
+    },
+    tags: 'leo odio porttitor id consequat in consequat',
+    isFeatured: false,
+    expDate: 1501272285833,
+    location: 'Aozai',
+  },
+  {
+    id: 11,
+    companyId: '95bs1pAVHNqr',
+    companyName: 'Riffwire',
+    info: {
+      title: 'Suspendisse potenti.',
+      description:
+        'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_536c0710b4804dba4e3b97995725b18e.jpg',
+      website: 'google.com.br',
+      social: 'amet lobortis sapien',
+    },
+    tags: 'ullamcorper purus sit amet nulla quisque arcu libero',
+    isFeatured: false,
+    expDate: 1501827457878,
+    location: 'Knurów',
+  },
+  {
+    id: 12,
+    companyId: '8AIIkDd',
+    companyName: 'Oozz',
+    info: {
+      title:
+        'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
+      description:
+        'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'rediff.com',
+      social: 'vestibulum',
+    },
+    tags: 'lacus morbi quis tortor',
+    isFeatured: true,
+    expDate: 1501806737356,
+    location: 'Agrínio',
+  },
+  {
+    id: 13,
+    companyId: 'rCbJtd49jxM',
+    companyName: 'Gabtype',
+    info: {
+      title: 'Ut at dolor quis odio consequat varius.',
+      description:
+        'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.\n\nMauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: '123-reg.co.uk',
+      social: 'ut suscipit a',
+    },
+    tags: 'consequat in consequat ut nulla',
+    isFeatured: false,
+    expDate: 1501708690462,
+    location: 'Piña',
+  },
+  {
+    id: 14,
+    companyId: 'tIpwW6M',
+    companyName: 'Twitternation',
+    info: {
+      title: 'In hac habitasse platea dictumst.',
+      description:
+        'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.\n\nAenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.\n\nCurabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_536c0710b4804dba4e3b97995725b18e.jpg',
+      website: 'edublogs.org',
+      social: 'tempus',
+    },
+    tags: 'porta volutpat quam',
+    isFeatured: false,
+    expDate: 1501244214625,
+    location: 'Bangelan',
+  },
+  {
+    id: 15,
+    companyId: 'YpEyjQPp',
+    companyName: 'Riffpedia',
+    info: {
+      title: 'In sagittis dui vel nisl.',
+      description:
+        'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'uol.com.br',
+      social: 'sit amet consectetuer',
+    },
+    tags: 'sit amet erat nulla tempus vivamus in felis',
+    isFeatured: true,
+    expDate: 1501204757270,
+    location: 'Al Fuwayliq',
+  },
+  {
+    id: 16,
+    companyId: 'GMkgfc',
+    companyName: 'Zoombox',
+    info: {
+      title: 'Suspendisse potenti.',
+      description:
+        'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.\n\nCum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_536c0710b4804dba4e3b97995725b18e.jpg',
+      website: 'cargocollective.com',
+      social: 'nisl',
+    },
+    tags: 'praesent',
+    isFeatured: true,
+    expDate: 1501323472436,
+    location: 'Khao Kho',
+  },
+  {
+    id: 17,
+    companyId: 'W3886LgKPnv',
+    companyName: 'Dynabox',
+    info: {
+      title: 'Phasellus sit amet erat.',
+      description:
+        'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'nytimes.com',
+      social: 'risus',
+    },
+    tags: 'imperdiet et commodo vulputate justo in',
+    isFeatured: true,
+    expDate: 1501245901434,
+    location: 'Aurora',
+  },
+  {
+    id: 18,
+    companyId: 'Kf17MFf8IUV7',
+    companyName: 'Rhynyx',
+    info: {
+      title: 'Etiam vel augue.',
+      description:
+        'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.\n\nNullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'nhs.uk',
+      social: 'pede justo',
+    },
+    tags: 'massa volutpat convallis morbi odio odio',
+    isFeatured: false,
+    expDate: 1501840202953,
+    location: 'Boavista',
+  },
+  {
+    id: 19,
+    companyId: 'yZSANf',
+    companyName: 'Zazio',
+    info: {
+      title: 'Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.',
+      description:
+        'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'umn.edu',
+      social: 'sed magna',
+    },
+    tags: 'nunc rhoncus dui vel sem sed sagittis nam congue risus',
+    isFeatured: true,
+    expDate: 1501713847540,
+    location: 'Jandira',
+  },
+  {
+    id: 20,
+    companyId: 'Q9OV4zT',
+    companyName: 'Wikizz',
+    info: {
+      title: 'In eleifend quam a odio.',
+      description:
+        'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.',
+      imgUrl:
+        'https://jobbatical.com/upload/thumbs/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'indiegogo.com',
+      social: 'et',
+    },
+    tags: 'mattis odio donec vitae nisi nam ultrices libero non mattis',
+    isFeatured: true,
+    expDate: 1501100333258,
+    location: 'Muzi',
+  },
+];

1d66fd4a1d5ffe8b93c28a2627877e7fa4284978
Viktors Jenovs <jviktors@gmail.com>
2017-08-01 11:40:32 +0300

    Change component structure (#37)

---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import './style.css';
+
+import Home from '../Home';
+import Jobs from '../Jobs';
+import Login from '../Login';
+import Join from '../Join';
+import Hiring from '../Hiring';
+
+const Main = props =>
+  <div>
+    <Route exact path="/" render={() => <Home data={props.data} />} />
+    <Route path="/jobs" component={Jobs} />
+    <Route path="/login" component={Login} />
+    <Route path="/join" component={Join} />
+    <Route path="/hiring" component={Hiring} />
+  </div>;
+
+export default Main;
--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './App.css';
-import Header from '../../components/Header';
+import api from '../../fakeApi';
+
 import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import Main from '../../components/Main';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  async getItems() {
+    let json = [];
+    try {
+      json = await api.getItems();
+      this.setState(() => ({
+        data: json.data,
+      }));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <Header />
-        <Footer />
-      </div>
+      <Router>
+        <div className="section">
+          <Header />
+          <Main data={this.state.data} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }

88ca5f179e12f60aa2ab30951002088c47b8d103
Viktors Jenovs <jviktors@gmail.com>
2017-08-06 11:19:45 +0300

    Feature/item card markup (#59)

---
+++ client/src/components/ItemDetails/dummyData.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    id: 1,
+    companyId: 'cGLSxH4',
+    companyName: 'StudyPortals',
+    info: {
+      title: 'Account Manager (Continental Europe)',
+      description: {
+        responsibilities: `* Praesent id massa id nisl venenatis lacinia.\
+        \n* Aenean sit amet justo.\
+        \n* In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.`,
+        requirements: `* Praesent id massa id nisl venenatis lacinia.\
+        \n* Aenean sit amet justo.\
+        \n* Morbi ut odio.\
+        \n* Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.\
+        \n* In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\
+        \n* Praesent id massa id nisl venenatis lacinia.\
+        \n* Aenean sit amet justo.\
+        \n* Morbi ut odio.\
+        \n* Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.`,
+        compensation: `* Praesent id massa id nisl venenatis lacinia.\
+        \n* Aenean sit amet justo.\
+        \n* Morbi ut odio.\
+        \n* Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.\
+        \n* In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.`,
+      },
+      imgUrl:
+        'https://jobbatical.com/upload/upload_898de66c5fa95ac4823f5947b09da911.jpg',
+      website: 'studyportals.com',
+      social: [
+        'facebook.com/studyportalscareers',
+        'twitter.com/@studyportalsdev',
+        'linkedin.com/company/studyportals',
+      ],
+    },
+    tags: ['Account', 'Manager', 'Europe', 'Sales', 'Spanish', 'German', 'EU'],
+    isFeatured: true,
+    expDate: 1504224000000,
+    location: 'Eindhoven, Netherlands',
+  },
+];

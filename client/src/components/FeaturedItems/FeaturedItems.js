06ea37d6c25466a4946d7f70cd939408c261622a
Viktors Jenovs <jviktors@gmail.com>
2017-08-16 17:03:45 +0300

    Refactor styles into styled components

--- client/src/components/FeaturedItems/FeaturedItems.js
+++ client/src/components/FeaturedItems/FeaturedItems.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './style.css';
+import { Wrapper } from './style.js';
 
 const FeaturedItems = ({ children }) =>
-  <div className="featured-items">
+  <Wrapper>
     {children}
-  </div>;
+  </Wrapper>;
 
 FeaturedItems.propTypes = {
   children: PropTypes.node.isRequired,
---
+++ client/src/components/FeaturedItems/style.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+import {
+  breakSmall,
+  breakMiddle,
+  breakLarge,
+} from './../../common-styles/layout';
+
+export const Wrapper = styled.div`
+  box-sizing: border-box;
+  padding: 0 15px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin: auto;
+  max-width: 1170px;
+
+  @media (min-width: ${breakSmall}px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 30px;
+    width: 750px;
+  }
+
+  @media (min-width: ${breakMiddle}px) {
+    width: 970px;
+  }
+
+  @media (min-width: ${breakLarge}px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    width: 1170px;
+  }
+`;

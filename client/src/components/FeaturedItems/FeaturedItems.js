65aecb1bf6d0ccc0e8cfd3f6525ef01e216517c4
Viktors Jenovs <jviktors@gmail.com>
2017-08-17 22:15:33 +0300

    Refactor data layout in components

--- client/src/components/FeaturedItems/FeaturedItems.js
+++ client/src/components/FeaturedItems/FeaturedItems.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ItemCard from '../ItemCard';
 import { Wrapper } from './style.js';
 
-const FeaturedItems = ({ children }) =>
+const parseImgUrl = url => url.replace(/upload\//, 'upload/thumbs/');
+
+const FeaturedItems = ({ data, ...props }) =>
   <Wrapper>
-    {children}
+    {data.map(item =>
+      <ItemCard
+        {...props}
+        key={item._id}
+        id={item._id}
+        imgUrl={item.info.imgUrl && parseImgUrl(item.info.imgUrl)}
+        title={item.info.title}
+        description={item.info.description}
+        expDate={item.expDate}
+        companyName={item.companyName}
+        location={item.location}
+      />
+    )}
   </Wrapper>;
 
 FeaturedItems.propTypes = {
-  children: PropTypes.node.isRequired,
+  data: PropTypes.array.isRequired,
 };
 
 export default FeaturedItems;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,28 +1,14 @@
 import React from 'react';
 
 import FeaturedItems from '../FeaturedItems';
-import ItemCard from '../ItemCard';
 import { HeroImage, Title, Wrapper } from './style';
-
-const parseImgUrl = url => url.replace(/upload\//, 'upload/thumbs/');
 
 const Home = ({ data, loading, ...props }) =>
   <Wrapper>
     <HeroImage />
     <Title>Featured Jobs</Title>
     {!loading
-      ? <FeaturedItems>
-          {data.map(item =>
-            <ItemCard
-              {...props}
-              {...item.info}
-              {...item}
-              key={item._id}
-              id={item._id}
-              imgUrl={item.info.imgUrl && parseImgUrl(item.info.imgUrl)}
-            />
-          )}
-        </FeaturedItems>
+      ? <FeaturedItems {...props} data={data} />
       : <div>Loading...</div>}
   </Wrapper>;
 

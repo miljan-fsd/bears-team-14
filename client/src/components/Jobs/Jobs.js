956794e7eeaf4f74dc45139254e2aafcf4deaa76
Viktors Jenovs <jviktors@gmail.com>
2017-09-02 13:19:36 +0300

    Add filtering

--- client/src/components/Jobs/Jobs.js
+++ client/src/components/Jobs/Jobs.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import FeaturedItems from '../FeaturedItems';
+import Search from '../Search';
 
-const Jobs = ({ data, ...props }) => <FeaturedItems data={data} {...props} />;
+const lowercaseArray = arr => arr.map(item => item.toLowerCase());
+
+class Jobs extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: props.data,
+      filteredData: props.data,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(() => ({
+      data: nextProps.data,
+      filteredData: nextProps.data,
+    }));
+  }
+
+  componentDidUpdate() {
+    // Trigger lazy loading
+    window.scrollBy(0, 1);
+    window.scrollBy(0, -1);
+  }
+
+  filterItems = (key = '') => {
+    const data = this.state.data;
+    if (!key.length)
+      return this.setState(() => ({
+        filteredData: data,
+      }));
+
+    const filteredData = data.filter(
+      item =>
+        (item.info &&
+          item.info.title &&
+          item.info.title.toLowerCase().includes(key)) ||
+        (item.tags && lowercaseArray(item.tags).includes(key)) ||
+        (item.location && item.location.toLowerCase().includes(key))
+    );
+
+    this.setState(() => ({
+      filteredData,
+    }));
+  };
+
+  render() {
+    const { filteredData } = this.state;
+    const { data, ...props } = this.props;
+    return (
+      <div>
+        <Search handleFilter={this.filterItems} />
+        <FeaturedItems data={filteredData} {...props} />
+      </div>
+    );
+  }
+}
 
 export default Jobs;

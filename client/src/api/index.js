96ef78c1637eaac524bdf21c76313fd74200babc
Viktors Jenovs <jviktors@gmail.com>
2017-09-03 14:11:58 +0300

    Create createNewJob api helper

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,10 @@
+import createNewJob from './createNewJob';
 import deleteItem from './deleteItem';
 import getItems from './getItems';
 import updateItem from './updateItem';
 
 export default {
+  createNewJob,
   deleteItem,
   getItems,
   updateItem,

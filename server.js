9a3ee2aa35fc935f34c0390115a17ff388afcb84
Viktors Jenovs <jviktors@gmail.com>
2017-09-07 14:04:39 +0300

    Add passport and session

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
 const express = require('express');
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const session = require('express-session');
 const mongoose = require('./server/mongoose');
 const bodyParser = require('body-parser');
 
@@ -14,14 +17,26 @@ app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
+  session({
+    secret: 'supersecret',
+    resave: true,
+    saveUninitialized: true,
   })
 );
 
 // ===== Models =====
 const Job = require('./server/models/Job.js');
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // ===== Routes =====
 const authRoute = require('./server/routes/authentication');
+const authController = require('./server/controllers/authController');
+const userController = require('./server/controllers/userController');
 const userHandler = require('./server/routes/userHandler');
 
 // ===== Server Setup =====
@@ -31,6 +46,24 @@ const PORT = process.env.PORT || 3001;
 //app.use(authRoute);
 app.use('/api/v1', userHandler);
 
+app.get('/test-login/', authController.isLoggedIn, (req, res) => {
+  res.send(`Logged in as ${req.user.username}`);
+});
+
+app.post(
+  '/register/',
+  userController.register,
+  authController.login,
+  (req, res) => {
+    res.json({ loggedIn: true });
+  }
+);
+
+app.post('/login/', authController.login, (req, res) => {
+  res.send({ loggedIn: true });
+});
+
+app.get('/logout/', authController.logout);
 // ====== Error Handler Middleware =====
 app.use(ErrorHandler);
 

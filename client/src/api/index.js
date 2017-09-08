6dfccb26e2958a4ffbd638cfded0c3ede6a62bd6
Viktors Jenovs <jviktors@gmail.com>
2017-09-08 12:48:28 +0300

    Create getUser api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,7 @@
 import createNewJob from './createNewJob';
 import deleteItem from './deleteItem';
 import getItems from './getItems';
+import getUser from './getUser';
 import loginUser from './loginUser';
 import updateItem from './updateItem';
 
@@ -8,6 +9,7 @@ export default {
   createNewJob,
   deleteItem,
   getItems,
+  getUser,
   loginUser,
   updateItem,
 };

eba0b950de1eba86da46eb32b6a555ea8b6c4f5a
Viktors Jenovs <jviktors@gmail.com>
2017-09-08 12:47:40 +0300

    Refactor SmallSave element into seperate component

---
+++ client/src/components/SmallSave/styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Save = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 8px;
+
+  &:hover {
+    color: #000;
+  }
+`;
+
+export const Bookmark = styled.span`
+  color: #bbb;
+  line-height: 12px;
+  margin-left: 6px;
+  width: 8px;
+
+  & > .fa {
+    font-size: 12px;
+  }
+
+  ${Save}:hover & {
+    color: #04d092;
+  }
+`;
